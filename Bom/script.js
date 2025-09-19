22000664d683cd1880ed79cacdb45b451e154b6e
pavanp0309 <pavanreddyp0309@gmail.com>
2025-09-19 13:15:24 +0530

    bom

---
+++ Bom/script.js
@@ -0,0 +1,45 @@
+console.log(window.location);
+console.log(location.port); //5500 , java/python :8080 8000 cutomised : 3000 5000  react:3000/5173
+console.log(location.protocol);
+// if(location.protocol=="http:"){
+//     confirm("these site is not secure do you want to proceed")
+// }
+// console.log(location.pathname)//
+// console.log(location.search)//
+// console.log(location.host)//
+// console.log(location.hostname)//
+// https://www.amazon.in/alm/storefront?almBrandId=ctnow&ref_=nav_cs_fresh
+
+// http://127.0.0.1:5500/Bom/index.html
+
+// Open the amazon and newspaper website  and viscode file
+// and get the details of  pathname search hostname origin port host href
+
+// amazon
+// href:
+// Pathname :  "/s"
+// search
+// origin
+// port
+// host
+// hostname
+
+// amazon 
+
+function handleClick(){
+    // console.log(location.href) //gets the data
+    // location.href="https://www.amazon.in/" //sets the data
+    // location.assign("https://www.amazon.in/")
+    // location.replace("https://www.amazon.in/")
+}
+
+// console.log(window.screen)
+// console.log(screen.availHeight)
+// console.log(screen.width)
+// console.log(screen.height)
+// console.log(screen.colorDepth)
+// console.log(screen.pixelDepth)
+// console.log(screen.orientation)
+// if(screen.availWidth<500){
+//     alert("please turn on desktop mode")
+// }
